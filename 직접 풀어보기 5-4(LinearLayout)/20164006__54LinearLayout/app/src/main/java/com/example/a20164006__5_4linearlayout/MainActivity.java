2b28137c1da4db1def0450327cb213a296a51d1a
hoon0303 <pbd2857@naver.com>
2021-05-06 23:41:31 +0900

    project 5-4

---
+++ 직접 풀어보기 5-4(LinearLayout)/20164006__54LinearLayout/app/src/main/java/com/example/a20164006__5_4linearlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20164006__5_4linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
