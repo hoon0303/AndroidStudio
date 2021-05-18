3ffa3d8e4435724f9d97e36b8fea6e244a8ede39
hoon0303 <pbd2857@naver.com>
2021-05-18 12:54:01 +0900

    tab host

---
+++ 탭호스트/tabhost_20164006/app/src/main/java/com/example/tabhost_20164006/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tabhost_20164006;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabSong);
+        tabHost.addTab(tabSpecSong);
+
+        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("ARTIST")
+                .setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
+
+        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
+    }
+}
